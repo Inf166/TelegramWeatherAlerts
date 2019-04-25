57c99249ec2d4f8f5cd3b90bc52d569dbcc34207
IceQ1337 <IceQ1337@users.noreply.github.com>
2019-04-25 15:10:13 +0200

    Portfolio + Init Prototyp v2

---
+++ Prototyp/Prototyp v1/server-module.js
@@ -0,0 +1,66 @@
+// RabbitMQ
+const amqp = require('amqplib/callback_api');
+
+// Globals
+const listener = 'server-module';
+const listener_topics = ['weather', 'telegram'];
+const topic = 'server';
+
+// General Channel Listener
+amqp.connect('amqp://localhost', function(err, conn) {
+    if (err) throw err;
+
+    conn.createChannel(function(err, channel) {
+        if (err) throw err;
+
+        channel.assertExchange(listener, 'topic', {
+            durable: false
+        });
+
+        channel.assertQueue('', {
+            exclusive: true
+        }, function(err, q) {
+            if (err) throw err;
+
+            console.log(' [*] Listening On: ' + listener);
+
+            listener_topics.forEach(function(key) {
+                channel.bindQueue(q.queue, listener, key);
+            });
+
+            // React to Messages
+            channel.consume(q.queue, function(msg) {
+                console.log(" [x] Received Via '%s':'%s'", msg.fields.routingKey, msg.content.toString());
+
+                // React to Weather Service
+                if (msg.fields.routingKey == 'weather') {
+                    console.log(' [*] Temperature has changed! Informing Telegram Users.');
+                    sendRMQMessage('telegram-module', `The temperature at the venue has changed to _${msg.content.toString()} °C_.`);
+                }
+            }, {
+                noAck: true
+            });
+        });
+    });
+});
+
+// Send Message to another Listener
+function sendRMQMessage(listener, msg) {
+    amqp.connect('amqp://localhost', function(err, connection) {
+        if (err) throw err;
+
+        connection.createChannel(function(err, channel) {
+            if (err) throw err;
+
+            channel.assertExchange(listener, 'topic', {
+                durable: false
+            });
+            channel.publish(listener, topic, Buffer.from(msg));
+            console.log(" [x] Sent To '%s' (%s): '%s'", listener, topic, msg);
+        });
+
+        setTimeout(function() {
+            connection.close();
+        }, 500);
+    });
+}
---
+++ Prototyp/Prototyp v2/.start-modules.bat
@@ -0,0 +1,4 @@
+start node server-module.js
+start node weather-module.js
+start node weather-check.js
+start node telegram-module.js
